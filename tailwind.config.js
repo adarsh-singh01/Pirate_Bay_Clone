13287075bd6e40e2913937367a0ed49247047e8e
Your Name <as8936940798@gmail.com>
2024-02-07 00:44:14 +0530

    complete

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens:{
+        'xsm':'300px',
+        'vsm':'400px',
+        'sm':'702px',
+    
+      }
+    },
+  },
+  plugins: [],
+}
+
